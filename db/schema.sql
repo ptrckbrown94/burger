e93940e6c68c3b8b965b063dc32a50400beca285
Pat Brown <ptrckbrown94@gmail.com>
2019-12-26 13:29:25 -0600

    added schema and installed required nodes

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+
+-- Drops the burger_db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "burger_db" database --
+CREATE DATABASE burgers_db;
+-- Makes it so all of the following code will affect burger_db --
+USE burgers_db;
+
+
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT
+burger_name varchar(30),
+devoured BOOLEAN NOT NULL,
+PRIMARY KEY (id)
+);
