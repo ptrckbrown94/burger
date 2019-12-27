cf22b9f29f052af8348df93f9c9d19a653df9a63
Pat Brown <ptrckbrown94@gmail.com>
2019-12-27 11:43:47 -0600

    updating routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,11 @@ const express = require("express")
 const burgers = require("../models/burger.js")
 const router = express.Router()
 
+router.get("/", function(req,res){
+    console.log("GET")
 
+    res.render("index")
+})
 
 
 
---
+++ public/assets/js/burger.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("addburger").addEventListener("click", function(){
+ const burgerName = docment.getElementById("burger-name").value
+ console.log(burgerName)
+})
